4a1ac099a34ccad2788215228d16e81d761d6163
RennanD <rplaykl@gmail.com>
2020-10-07 22:41:18 -0300

    mobile - feat: Init Mobile app

---
+++ mobile/src/index.js
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Index = () => {
+  return <View />;
+}
+
+export default Index;

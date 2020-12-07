b9ef37e4d30bb54657a5b9bb93d44aafbb41b194
RennanD <rplaykl@gmail.com>
2020-12-06 21:52:48 -0300

    fix: Convert value

--- mobile/src/utils/formatBalance.js
+++ mobile/src/utils/formatBalance.js
@@ -1,7 +1,9 @@
-const formatValue = (value) =>
-  Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-  }).format(value);
+import Intl from 'intl';
+import ptBR from 'intl/locale-data/jsonp/pt-BR';
+
+const { format: formatValue } = new Intl.NumberFormat(ptBR, {
+  style: 'currency',
+  currency: 'BRL',
+});
 
 export default formatValue;
